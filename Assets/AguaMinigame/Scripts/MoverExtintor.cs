403f14c32cf81af40ba23618bcac3ed87da3d015
Rapoporras <nacho.porras@lgfm.app>
2024-07-26 22:20:05 +0200

    mover mirilla con tactil

--- Assets/AguaMinigame/Scripts/MoverExtintor.cs
+++ Assets/AguaMinigame/Scripts/MoverExtintor.cs
@@ -12,13 +12,15 @@ public class MoverExtintor : MonoBehaviour
 	private Vector2 position;
 	private InputManager inputManager;
 	[SerializeField] private float speedMove = 20f;
+
+	public Camera UpperCamera;
 	private void Awake()
 	{
 		inputManager = input.GetComponent<InputManager>();
 	}
 	void Start()
 	{
-		position = inputManager.GetPosition();
+		position = transform.position;
 	}
 
 	// Update is called once per frame
@@ -26,7 +28,25 @@ public class MoverExtintor : MonoBehaviour
 	{
 		position = inputManager.GetPosition();
 		Debug.Log("Mirilla" + position.x + " " + position.y);
-		
+		var step = speedMove * Time.deltaTime; // calculate distance to move
+		Vector3 newPosition;
+		if (position.x > 0 || position.y > 0)
+		{
 
+			newPosition = UpperCamera.ScreenToWorldPoint(new Vector3(position.x, position.y, 0));
+
+		}
+		else
+		{
+
+			newPosition = UpperCamera.ScreenToWorldPoint(new Vector3(0, 0, 0));
+
+
+		}
+
+
+		transform.position = Vector3.MoveTowards(transform.position, new Vector3(newPosition.x, newPosition.y, 0), step);
 	}
+
 }
+
