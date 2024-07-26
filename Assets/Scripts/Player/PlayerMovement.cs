82cd8af4f6a647c14386aadcf7310fa53b1f0d3d
JavierStark <javiertorralbocortes@gmail.com>
2024-07-26 20:54:47 +0200

    fix: havePenalty with nintendo input

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -26,19 +26,57 @@ public class PlayerMovement : MonoBehaviour
         var pad = GamePad.CirclePad.normalized;
         var cross = GetCross();
 
-        if (GamePad.GetButtonTrigger(N3dsButton.L))
+        if (GamePad.GetButtonTrigger(N3dsButton.L) || Input.GetKeyDown(KeyCode.B))
         {
             havePenalty = !havePenalty;
         }
 
 #if UNITY_EDITOR
-        pad = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        // cross = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if(Input.GetKey(KeyCode.A))
+        {
+            pad = Vector2.left;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            pad = Vector2.right;
+        }
+        else if(Input.GetKey(KeyCode.W))
+        {
+            pad = Vector2.up;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            pad = Vector2.down;
+        }
+        else{
+            pad = Vector2.zero;
+        }
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            cross = Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            cross = Vector2.down;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            cross = Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            cross = Vector2.right;
+        }
+        else
+        {
+            cross = Vector2.zero;
+        }
 #endif
 
         if (havePenalty)
         {
-            input += new Vector2(cross.x, pad.y);
+            input = new Vector2(cross.x, pad.y);
         }
         else
         {
@@ -53,11 +91,6 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-    void FixedUpdate()
-    {
-
-
-    }
     private static Vector2 GetCross()
     {
         var cross = new Vector2();
