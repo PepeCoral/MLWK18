f1a510b659689be792c4f3ad0799aafda2922422
Rapoporras <nacho.porras@lgfm.app>
2024-07-27 00:10:45 +0200

    move manguera

--- Assets/AguaMinigame/Scripts/MoverExtintor.cs
+++ Assets/AguaMinigame/Scripts/MoverExtintor.cs
@@ -14,20 +14,23 @@ public class MoverExtintor : MonoBehaviour
 	[SerializeField] private float speedMove = 20f;
 
 	public Camera UpperCamera;
+
+	public GameObject fuegoDerecha;
+	public GameObject fuegoIzquierda;
 	private void Awake()
 	{
 		inputManager = input.GetComponent<InputManager>();
 	}
 	void Start()
 	{
-		position = transform.position;
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		position = inputManager.GetPosition();
-		Debug.Log("Mirilla" + position.x + " " + position.y);
+		// Debug.Log("Mirilla" + position.x + " " + position.y);
 		var step = speedMove * Time.deltaTime; // calculate distance to move
 		Vector3 newPosition;
 		if (position.x > 0 || position.y > 0)
@@ -39,14 +42,39 @@ public class MoverExtintor : MonoBehaviour
 		else
 		{
 
-			newPosition = UpperCamera.ScreenToWorldPoint(new Vector3(0, 0, 0));
+			newPosition = new Vector3(0, 0, 0);
 
 
 		}
 
+		// float rotation = newPosition.x * Time.deltaTime * speedMove;
 
-		transform.position = Vector3.MoveTowards(transform.position, new Vector3(newPosition.x, newPosition.y, 0), step);
+		// rotation = Vector3.Angle(position, newPosition);
+		// transform.rotation = Quaternion.Euler(0, 0, rotation);
+
+		Vector3 direction = (newPosition - transform.position).normalized;
+
+		// Calcula la rotación hacia la nueva dirección
+		Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, direction);
+
+		// Interpola suavemente la rotación
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * speedMove);
+
+
+		if (targetRotation.z > 0)
+		{
+
+			fuegoDerecha.SetActive(false);
+		}
+		else
+		{
+
+
+			fuegoIzquierda.SetActive(false);
+		}
 	}
+
+
 
 }
 
