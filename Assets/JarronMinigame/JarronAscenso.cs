4b02517cd6bdeaec231d28fe050df0a0836d8027
Pepe <coritochico@gmail.com>
2024-07-27 01:04:22 +0200

    comit

---
+++ Assets/JarronMinigame/JarronAscenso.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JarronAscenso : MonoBehaviour
+{
+
+    [SerializeField] float _speed;
+
+    [SerializeField] Vector3 _startingPos;
+    [SerializeField] float _maxHeigh;
+
+    private bool _isPlaying = true;
+
+
+    void Start()
+    {
+        transform.position = _startingPos;
+    }
+
+
+    void Update()
+    {
+        if (transform.position.y < _maxHeigh)
+        {
+            transform.position += Vector3.up * _speed * Time.deltaTime;
+        }
+        else if (_isPlaying)
+        {
+            _isPlaying = false;
+        }
+
+    }
+}
---
+++ Assets/JarronMinigame/JarronGiroscopio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JarronGiroscopio : MonoBehaviour
+{
+    Gyroscope gyro;
+
+    private void Start()
+    {
+        gyro = Input.gyro;
+        gyro.enabled = true;
+    }
+
+    private void Update()
+    {
+
+        float yRotation = gyro.attitude.eulerAngles.y;
+
+    }
+}
