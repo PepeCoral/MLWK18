c25a15001e3876574d08f9e2667960c337aca314
JavierStark <javiertorralbocortes@gmail.com>
2024-07-26 20:06:47 +0200

    feat: pad and cross input

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.N3DS;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private bool havePenalty = false;
+    private Vector2 input;
 
     void Update()
     {
+        var pad = GamePad.CirclePad.normalized;
+        var cross = GetCross();
 
+        if (havePenalty)
+        {
+            input += new Vector2(cross.x, pad.y);
+        }
+        else
+        {
+            input = pad + cross;
+        }
+        
+        input = input.normalized;
+    }
+
+    private static Vector2 GetCross()
+    {
+        var cross = new Vector2();
+        if (GamePad.GetButtonHold(N3dsButton.Up))
+        {
+            cross += Vector2.up;
+        }
+
+        if (GamePad.GetButtonHold(N3dsButton.Down))
+        {
+            cross += Vector2.down;
+        }
+
+        if (GamePad.GetButtonHold(N3dsButton.Left))
+        {
+            cross += Vector2.left;
+        }
+
+        if (GamePad.GetButtonHold(N3dsButton.Right))
+        {
+            cross += Vector2.right;
+        }
+
+        return cross;
     }
 }
