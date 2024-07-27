41cfedb122c69b226894d40a6eff45f863ada884
Pepe <coritochico@gmail.com>
2024-07-27 11:29:40 +0200

    cosa

--- Assets/CableMinigame/Scripts/CableMinigameManager.cs
+++ Assets/CableMinigame/Scripts/CableMinigameManager.cs
@@ -1,77 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using NUnit.Framework.Constraints;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CableMinigameManager : MinigameManager
 {
-	[Header("Cable Minigame Parameters")]
-	[SerializeField] private CableStartLocation[] AvailableCables;
-	[SerializeField] private Fuse[] Fuses;
+    [Header("Cable Minigame Parameters")]
+    [SerializeField] private CableStartLocation[] AvailableCables;
+    [SerializeField] private Fuse[] Fuses;
 
-	[Header("UI Information")] 
-	[SerializeField] private Text TimerText;
-	
-	// Update is called once per frame
-	protected override void Update ()
-	{
-		base.Update();
+    [Header("UI Information")]
+    [SerializeField] private Text TimerText;
 
-		if (IsMinigameActive())
-		{
-			if (CurrentTimeLength > 0)
-			{
-				TimerText.text = CurrentTimeLength.ToString("0.00");
-			}
-		}
-		else
-		{
-			if (CurrentStartCountdown > 0)
-			{
-				TimerText.text = "Game about to Start " + CurrentStartCountdown;
-			}
-		}
-	}
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
 
-	protected override void OnGameTimerExpired()
-	{
-		base.OnGameTimerExpired();
-		
-		TimerText.text = "Game Timed Out!!!";
-	}
-	
-	public override bool CanMinigameEnd()
-	{
-		int expectedAmount = 0;
-		foreach (CableStartLocation cable in AvailableCables)
-		{
-			if (cable.isCompleted)
-			{
-				expectedAmount++;
-			}
-		}
+        if (IsMinigameActive())
+        {
+            if (CurrentTimeLength > 0)
+            {
+                TimerText.text = CurrentTimeLength.ToString("0.00");
+            }
+        }
+        else
+        {
+            if (CurrentStartCountdown > 0)
+            {
+                TimerText.text = "Game about to Start " + CurrentStartCountdown;
+            }
+        }
+    }
 
-		if (expectedAmount != AvailableCables.Length)
-		{
-			return false;
-		}
-		
-		expectedAmount = 0;
-		foreach (Fuse fuse in Fuses)
-		{
-			if (fuse.isSlotted)
-			{
-				expectedAmount++;
-			}
-		}
+    protected override void OnGameTimerExpired()
+    {
+        base.OnGameTimerExpired();
 
-		return expectedAmount == Fuses.Length;
-	}
+        TimerText.text = "Game Timed Out!!!";
+    }
 
-	public override void CompleteMinigame()  
-	{
-		base.CompleteMinigame();
-		TimerText.text = "Game COMPLETED Out!!!";
-	}
+    public override bool CanMinigameEnd()
+    {
+        int expectedAmount = 0;
+        foreach (CableStartLocation cable in AvailableCables)
+        {
+            if (cable.isCompleted)
+            {
+                expectedAmount++;
+            }
+        }
+
+        if (expectedAmount != AvailableCables.Length)
+        {
+            return false;
+        }
+
+        expectedAmount = 0;
+        foreach (Fuse fuse in Fuses)
+        {
+            if (fuse.isSlotted)
+            {
+                expectedAmount++;
+            }
+        }
+
+        return expectedAmount == Fuses.Length;
+    }
+
+    public override void CompleteMinigame()
+    {
+        base.CompleteMinigame();
+        TimerText.text = "Game COMPLETED Out!!!";
+    }
 }
