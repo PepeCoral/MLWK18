906f4cd4b09a0af5f11058200ba3315101a6be43
Pepe <coritochico@gmail.com>
2024-07-28 01:49:01 +0200

    aa

--- Assets/Scripts/Shaking.cs
+++ Assets/Scripts/Shaking.cs
@@ -3,29 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shaking : MonoBehaviour {
-	Gyroscope gyro;
-	public bool isShaking = false;
-	
-	CastigoHumo castigoHumo;
+public class Shaking : MonoBehaviour
+{
+    Gyroscope gyro;
+    public bool isShaking = false;
 
-	private void Start()
-	{
-		gyro = Input.gyro;
-		gyro.enabled = true;
-		
-		castigoHumo = GetComponent<CastigoHumo>();
-	}
+    CastigoHumo castigoHumo;
 
-	private void Update()
-	{
-		isShaking = DetectShaking();
-		
-		castigoHumo.blow = isShaking;
-	}
+    private void Start()
+    {
+        gyro = Input.gyro;
+        gyro.enabled = true;
 
-	private bool DetectShaking()
-	{
-		return gyro.userAcceleration.magnitude > 0.7f;
-	}
+        castigoHumo = GetComponent<CastigoHumo>();
+    }
+
+    private void Update()
+    {
+        //isShaking = DetectShaking();
+
+        castigoHumo.blow = isShaking;
+    }
+
+    private bool DetectShaking()
+    {
+        return gyro.userAcceleration.magnitude > 0.7f;
+    }
 }
