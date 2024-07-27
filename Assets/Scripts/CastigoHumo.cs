e72161301e7858036f267620e885547d624d566a
JavierStark <javiertorralbocortes@gmail.com>
2024-07-27 19:52:00 +0200

    feat: smoke can be got rid of during some seconds

--- Assets/Scripts/CastigoHumo.cs
+++ Assets/Scripts/CastigoHumo.cs
@@ -17,36 +17,81 @@ public class CastigoHumo : MonoBehaviour
 	public GameObject humoPart7;
 	public GameObject humoPart8;
 	public bool blow = false;
+	
+	[SerializeField] private int minTime = 5;
+	[SerializeField] private int maxTime = 10;
+	[SerializeField] private float blowForce = 0.2f;
+
+	private int timeToSmoke;
+	private float currentTime = 0;
+	
+	private bool smoking = false;
+	
+	private float currentPercentage = 0;
+	
+	private bool activationCompleted = false;
+	
 	void Start()
 	{
-
+		timeToSmoke = Random.Range(minTime, maxTime);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if(!smoking)
+		{
+			activationCompleted = false;
+			currentTime += Time.deltaTime;
 
-		if (blow)
-		{
-			humoPart1.transform.DOMove(humoPart1.GetComponent<ParteHumo>().endPosition, 2f).SetEase(Ease.Linear);
-			humoPart2.transform.DOMove(humoPart2.GetComponent<ParteHumo>().endPosition, 2f).SetEase(Ease.Linear);
-			humoPart3.transform.DOMove(humoPart3.GetComponent<ParteHumo>().endPosition, 2f).SetEase(Ease.Linear);
-			humoPart4.transform.DOMove(humoPart4.GetComponent<ParteHumo>().endPosition, 2f).SetEase(Ease.Linear);
-			humoPart5.transform.DOMove(humoPart5.GetComponent<ParteHumo>().endPosition, 2f).SetEase(Ease.Linear);
-			humoPart6.transform.DOMove(humoPart6.GetComponent<ParteHumo>().endPosition, 2f).SetEase(Ease.Linear);
-			humoPart7.transform.DOMove(humoPart7.GetComponent<ParteHumo>().endPosition, 2f).SetEase(Ease.Linear);
-			humoPart8.transform.DOMove(humoPart8.GetComponent<ParteHumo>().endPosition, 2f).SetEase(Ease.Linear);
+			if (currentTime >= timeToSmoke)
+			{
+				currentTime = 0;
+				timeToSmoke = Random.Range(minTime, maxTime);
+				ActivateSmoke();
+				smoking = true;
+			}
+
+			return;
 		}
-		else
+		
+		if (blow && activationCompleted)
 		{
-			humoPart1.transform.DOMove(humoPart1.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
-			humoPart2.transform.DOMove(humoPart2.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
-			humoPart3.transform.DOMove(humoPart3.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
-			humoPart4.transform.DOMove(humoPart4.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
-			humoPart5.transform.DOMove(humoPart5.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
-			humoPart6.transform.DOMove(humoPart6.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
-			humoPart7.transform.DOMove(humoPart7.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
-			humoPart8.transform.DOMove(humoPart8.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
+			currentPercentage += Time.deltaTime * blowForce;
+			
+			SetSmokeToPercentage(currentPercentage);
 		}
+	}
+
+	private void SetSmokeToPercentage(float f)
+	{
+		if(f >= 1)
+		{
+			blow = false;
+			currentPercentage = 0;
+			smoking = false;
+		}
+		
+		humoPart1.transform.position = Vector3.Lerp(humoPart1.GetComponent<ParteHumo>().initPosition, humoPart1.GetComponent<ParteHumo>().endPosition, f);
+		humoPart2.transform.position = Vector3.Lerp(humoPart2.GetComponent<ParteHumo>().initPosition, humoPart2.GetComponent<ParteHumo>().endPosition, f);
+		humoPart3.transform.position = Vector3.Lerp(humoPart3.GetComponent<ParteHumo>().initPosition, humoPart3.GetComponent<ParteHumo>().endPosition, f);
+		humoPart4.transform.position = Vector3.Lerp(humoPart4.GetComponent<ParteHumo>().initPosition, humoPart4.GetComponent<ParteHumo>().endPosition, f);
+		humoPart5.transform.position = Vector3.Lerp(humoPart5.GetComponent<ParteHumo>().initPosition, humoPart5.GetComponent<ParteHumo>().endPosition, f);
+		humoPart6.transform.position = Vector3.Lerp(humoPart6.GetComponent<ParteHumo>().initPosition, humoPart6.GetComponent<ParteHumo>().endPosition, f);
+		humoPart7.transform.position = Vector3.Lerp(humoPart7.GetComponent<ParteHumo>().initPosition, humoPart7.GetComponent<ParteHumo>().endPosition, f);
+		humoPart8.transform.position = Vector3.Lerp(humoPart8.GetComponent<ParteHumo>().initPosition, humoPart8.GetComponent<ParteHumo>().endPosition, f);
+	}
+
+	private void ActivateSmoke()
+	{
+		humoPart1.transform.DOMove(humoPart1.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
+		humoPart2.transform.DOMove(humoPart2.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
+		humoPart3.transform.DOMove(humoPart3.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
+		humoPart4.transform.DOMove(humoPart4.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
+		humoPart5.transform.DOMove(humoPart5.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
+		humoPart6.transform.DOMove(humoPart6.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
+		humoPart7.transform.DOMove(humoPart7.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear);
+		humoPart8.transform.DOMove(humoPart8.GetComponent<ParteHumo>().initPosition, 2f).SetEase(Ease.Linear).OnComplete(() => activationCompleted = true);
+
 	}
 }
