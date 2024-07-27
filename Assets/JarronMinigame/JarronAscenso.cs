30c1e0b22e253029bff5f35d9aae8e0c544987e2
Pepe <coritochico@gmail.com>
2024-07-27 12:49:15 +0200

    jarron ascenso

--- Assets/JarronMinigame/JarronAscenso.cs
+++ Assets/JarronMinigame/JarronAscenso.cs
@@ -8,18 +8,35 @@ public class JarronAscenso : MonoBehaviour
     [SerializeField] float _speed;
     [SerializeField] float _minHeigh;
 
-    private bool _isPlaying = true;
+    private bool _isPlaying = false;
 
     void Update()
     {
-        if (transform.position.y < _minHeigh)
+        if (transform.position.y > _minHeigh && _isPlaying)
         {
             transform.position += Vector3.down * (_speed * Time.deltaTime);
         }
         else if (_isPlaying)
         {
+            print("terminado");
             _isPlaying = false;
         }
 
     }
+
+    public void StartGame()
+    {
+        _isPlaying = true;
+    }
+
+    public void CompleteMinigame()
+    {
+        _isPlaying = false;
+    }
+
+    public void JarronFall()
+    {
+    }
+
+
 }
--- Assets/Scripts/MinigameManager.cs
+++ Assets/Scripts/MinigameManager.cs
@@ -3,78 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class MinigameManager : MonoBehaviour {
-	
-	[FormerlySerializedAs("TimeToFinish")]
-	[Header("Generic Options")]
-	//Time to finish this minigame once started, if this expires, the minigame has failed
-	[SerializeField] private float MinigameTimeLength = 15;
-	[SerializeField] private float CountdownToStart = 5;
+public class MinigameManager : MonoBehaviour
+{
+
+    [FormerlySerializedAs("TimeToFinish")]
+    [Header("Generic Options")]
+    //Time to finish this minigame once started, if this expires, the minigame has failed
+    [SerializeField] private float MinigameTimeLength = 15;
+    [SerializeField] private float CountdownToStart = 5;
 
 
-	protected float CurrentStartCountdown;
-	protected float CurrentTimeLength;
-	
-	private bool bIsMinigameTimerActive = false;
-	private bool bIsStartCountdownActive = true;
-	
-	// Use this for initialization
-	void Awake ()
-	{
-		CurrentTimeLength = MinigameTimeLength;
-		CurrentStartCountdown = CountdownToStart;
-	}
+    protected float CurrentStartCountdown;
+    protected float CurrentTimeLength;
 
-	protected virtual void StartMinigame()
-	{
-		bIsMinigameTimerActive = true;
-		
-	}
-	
-	// Update is called once per frame
-	protected virtual void Update () {
-		if (bIsMinigameTimerActive)
-		{
-			CurrentTimeLength -= Time.deltaTime;
+    private bool bIsMinigameTimerActive = false;
+    private bool bIsStartCountdownActive = true;
 
-			if (CurrentTimeLength <= 0)
-			{
-				CurrentTimeLength = 0;
-				OnGameTimerExpired();
-			}
-		}
+    // Use this for initialization
+    void Awake()
+    {
+        CurrentTimeLength = MinigameTimeLength;
+        CurrentStartCountdown = CountdownToStart;
+    }
 
-		if (bIsStartCountdownActive)
-		{
-			CurrentStartCountdown -= Time.deltaTime;
+    protected virtual void StartMinigame()
+    {
+        bIsMinigameTimerActive = true;
 
-			if (CurrentStartCountdown <= 0)
-			{
-				CurrentStartCountdown = 0;
-				bIsStartCountdownActive = false;
-				
-				StartMinigame();
-			}
-		}
-	}
+    }
 
-	protected virtual void OnGameTimerExpired()
-	{
-		bIsMinigameTimerActive = false;
-	}
-	
-	public virtual bool IsMinigameActive()
-	{
-		return bIsMinigameTimerActive;
-	}
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (bIsMinigameTimerActive)
+        {
+            CurrentTimeLength -= Time.deltaTime;
 
-	public virtual bool CanMinigameEnd()
-	{
-		return true;
-	}
+            if (CurrentTimeLength <= 0)
+            {
+                CurrentTimeLength = 0;
+                OnGameTimerExpired();
+            }
+        }
 
-	public virtual void CompleteMinigame()
-	{
-		bIsMinigameTimerActive = false;
-	}
+        if (bIsStartCountdownActive)
+        {
+            CurrentStartCountdown -= Time.deltaTime;
+
+            if (CurrentStartCountdown <= 0)
+            {
+                CurrentStartCountdown = 0;
+                bIsStartCountdownActive = false;
+
+                StartMinigame();
+            }
+        }
+    }
+
+    protected virtual void OnGameTimerExpired()
+    {
+        bIsMinigameTimerActive = false;
+    }
+
+    public virtual bool IsMinigameActive()
+    {
+        return bIsMinigameTimerActive;
+    }
+
+    public virtual bool CanMinigameEnd()
+    {
+        return true;
+    }
+
+    public virtual void CompleteMinigame()
+    {
+        bIsMinigameTimerActive = false;
+    }
 }
