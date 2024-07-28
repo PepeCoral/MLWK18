3075a307e3829f0ccb01b5fab030f70fe0979e47
Pepe <coritochico@gmail.com>
2024-07-28 13:07:56 +0200

    AAAAAA G

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,60 +5,59 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.N3DS;
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
-	public GameObject bombilla;
-	private Animator bombillaAnimation;
-	
-	private Button playButton;
-	
-	[SerializeField] private Canvas canvas;
+    public GameObject bombilla;
+    private Animator bombillaAnimation;
 
-	private void Awake()
-	{
-		playButton = FindObjectsOfType<Button>().FirstOrDefault(g => g.name == "PlayButton");
-		
-		if (bombilla != null)
-		{
-			bombillaAnimation = bombilla.GetComponent<Animator>();
+    private Button playButton;
 
-		}
+    [SerializeField] private Canvas canvas;
 
-	}
+    private void Awake()
+    {
+        playButton = FindObjectsOfType<Button>().FirstOrDefault(g => g.name == "PlayButton");
 
-	private void Start()
-	{
-		canvas.sortingOrder = 1000;
-		playButton.transform.DOScale(new Vector3(1.1f, 1.1f, 1.1f), 1).SetLoops(-1, LoopType.Yoyo);
-	}
+        if (bombilla != null)
+        {
+            bombillaAnimation = bombilla.GetComponent<Animator>();
 
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Space) || GamePad.GetButtonTrigger(N3dsButton.A))
-		{
-			StartGame();
-		}
-	}
+        }
+
+    }
+
+    private void Start()
+    {
+        canvas.sortingOrder = 1000;
+        //playButton.transform.DOScale(new Vector3(1.1f, 1.1f, 1.1f), 1).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || GamePad.GetButtonTrigger(N3dsButton.A))
+        {
+            StartGame();
+        }
+    }
 
 
-	public void StartGame()
-	{
-		canvas.sortingOrder = -1000;
-		if (bombillaAnimation != null)
-		{
-			StartCoroutine(WaitForAnimationAndSwitchScene());
-		}
-	}
-	private IEnumerator WaitForAnimationAndSwitchScene()
-	{
-		bombillaAnimation.SetBool("start", true);
+    public void StartGame()
+    {
+        canvas.sortingOrder = -1000;
+        if (bombillaAnimation != null)
+        {
+            StartCoroutine(WaitForAnimationAndSwitchScene());
+        }
+    }
+    private IEnumerator WaitForAnimationAndSwitchScene()
+    {
+        bombillaAnimation.SetBool("start", true);
 
-		// Wait for 1 second
-		yield return new WaitForSeconds(1f);
+        // Wait for 1 second
+        yield return new WaitForSeconds(1f);
 
-		// Switch to the next scene
-		SceneSwitcher.Instance.SwitchToNextScene();
-	}
+        // Switch to the next scene
+        SceneSwitcher.Instance.SwitchToNextScene();
+    }
 }
