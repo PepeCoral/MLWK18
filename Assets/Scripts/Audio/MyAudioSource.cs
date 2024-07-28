517fe6130178289f09c44ac266eb72cf172ac525
Marco Exp <marcoexp@ucm.es>
2024-07-28 15:47:30 +0200

    Add music assets

---
+++ Assets/Scripts/Audio/MyAudioSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MyAudioSource : MonoBehaviour
+{
+    public static MyAudioSource Instance { get; private set; }
+    AudioSource AS;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            AS = GetComponent<AudioSource>();
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void PlaySoundEffect(AudioClip SFX)
+    {
+        AS.PlayOneShot(SFX);
+    }
+
+    public void PlayMusic(AudioClip clip, bool doLoop)
+    {
+        AS.clip = clip;
+        AS.loop = doLoop;
+        AS.Play();
+    }
+
+    public void StopMusic()
+    {
+        AS.Stop();
+    }
+}
---
+++ Assets/Scripts/Audio/PlayMusicOnLoad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusicOnLoad : MonoBehaviour
+{
+
+	[SerializeField] private AudioClip musicClip;
+	[SerializeField] private bool StopMusicOnDestroy = false;
+	
+	void Start () {
+		MyAudioSource.Instance.PlayMusic(musicClip, true);
+	}
+
+	private void OnDestroy()
+	{
+		if (StopMusicOnDestroy)
+		{
+			MyAudioSource.Instance.StopMusic();
+		}
+	}
+}
