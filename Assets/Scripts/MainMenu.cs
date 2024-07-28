d0a0ddc1b3291721c9a3a3af655c5997faed445b
Rapoporras <nacho.porras@lgfm.app>
2024-07-28 04:07:50 +0200

    menu animacion lampara

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,54 +4,51 @@ using UnityEngine;
 using UnityEngine.N3DS;
 using UnityEngine.UI;
 using DG.Tweening;
+
 public class MainMenu : MonoBehaviour
 {
-
-	// Use this for initialization
-
 	public Text text;
 	public GameObject bombilla;
-
-	private SpriteRenderer spriteRenderer;
-	private GameObject scenemanager;
-
-	public float step = 0f;
-
-
+	private Animator bombillaAnimation;
 
 	private void Awake()
 	{
-		scenemanager = GameObject.FindWithTag("SceneSwitcher");
+		if (bombilla != null)
+		{
+			bombillaAnimation = bombilla.GetComponent<Animator>();
+
+		}
+
 	}
+
 	void Start()
 	{
-		text.DOFade(0f, 1f).SetLoops(-1, LoopType.Yoyo);
-		spriteRenderer = bombilla.GetComponent<SpriteRenderer>();
+		if (text != null)
+		{
+			text.DOFade(0f, 1f).SetLoops(-1, LoopType.Yoyo);
+		}
+
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-
 		if (Input.GetKeyDown(KeyCode.Space) || GamePad.GetButtonTrigger(N3dsButton.A))
 		{
-			Sequence mySequence = DOTween.Sequence();
-			mySequence.Append(spriteRenderer.DOColor(new Color(255f, 255f, 255f, 255f), 0.1f));
-			if (step < 2)
+			if (bombillaAnimation != null)
 			{
-				mySequence.Append(spriteRenderer.DOColor(new Color(255f, 255f, 0f, 255f), 0.1f));
+				StartCoroutine(WaitForAnimationAndSwitchScene());
 			}
-			mySequence.PrependInterval(1);
-			step++;
 		}
+	}
 
+	private IEnumerator WaitForAnimationAndSwitchScene()
+	{
+		bombillaAnimation.SetBool("start", true);
 
-		if (step == 3)
-		{
-			scenemanager.GetComponent<SceneSwitcher>().SwitchToNextScene();
-			step++;
-		}
+		// Wait for 1 second
+		yield return new WaitForSeconds(1f);
 
-		// text.DOFade(0f, 1f).SetLoops(-1, LoopType.Yoyo);
+		// Switch to the next scene
+		SceneSwitcher.Instance.SwitchToNextScene();
 	}
 }
